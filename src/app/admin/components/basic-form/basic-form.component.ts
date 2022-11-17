854cf90ba468af11e1210f2c555d77e9937fc4f5
AndrezJimenez <andru456@hotmail.com>
2022-11-16 20:38:35 -0500

    formgroup

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators,FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-form',
@@ -8,24 +8,43 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class BasicFormComponent implements OnInit {
 
-  nameField = new FormControl('',[Validators.required, Validators.maxLength(2)]);
-  emailField = new FormControl('');
-  phoneField = new FormControl('');
+  form = new FormGroup ({
+    name: new FormControl('',[Validators.required, Validators.maxLength(2)]),
+    email: new FormControl(''),
+    phone:new FormControl(''),
+    color:new FormControl(''),
+    date:new FormControl(''),
+    number:new FormControl(''),
+    category: new FormControl('category-1'),
+    tag: new FormControl(''),
+    agree:new FormControl (false),
+    gender:new FormControl(''),
+    zone:new FormControl(''),
+    preferences:new FormControl(''),
+  } );
 
-  colorField = new FormControl('');
-  dateField = new FormControl('');
-  numberField = new FormControl('');
-  categoryField = new FormControl('category-1');
-  tagField = new FormControl('');
+  // nameField = new FormControl('',[Validators.required, Validators.maxLength(2)]);
+  // emailField = new FormControl('');
+  // phoneField = new FormControl('');
 
-  agreefield = new FormControl (false);
+  // colorField = new FormControl('');
+  // dateField = new FormControl('');
+  // numberField = new FormControl('');
+  // categoryField = new FormControl('category-1');
+  // tagField = new FormControl('');
 
-  genderField = new FormControl ('');
-  zoneField = new FormControl ('');
-  preferencesField = new FormControl ('');
+  // agreefield = new FormControl (false);
+
+  // genderField = new FormControl ('');
+  // zoneField = new FormControl ('');
+  // preferencesField = new FormControl ('');
 
 
   constructor() { }
+
+  get nameField(){
+    return this.form.get('name');
+  }
 
   ngOnInit(): void {
     this.nameField.valueChanges
@@ -37,6 +56,7 @@ export class BasicFormComponent implements OnInit {
   getNameValue (){
     console.log(this.nameField.value);
   }
+
 
   
 
